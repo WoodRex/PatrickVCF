4f0bda4635c6fc33cf84b6179e597602d4dd0fe9
Woodrex <woodrex83@gmail.com>
2023-01-11 16:16:53 +0800

    check remark

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ import { getFinvizScreen } from "node-finviz-screener";
 import * as fs from 'fs';
 import yahooFinance from "yahoo-finance2";
 
+// Summary detail missing SMA 150
+const queryOptions = { modules: ['price', 'summaryDetail'] };
 
 getFinvizScreen('https://finviz.com/screener.ashx?v=111&f=cap_mid,sh_price_o15')
 .then((res) => {
-  Promise.all(res.map(symbol => yahooFinance.quoteSummary(symbol)))
+  Promise.all(res.map(symbol => yahooFinance.quoteSummary(symbol, queryOptions)))
   .then((data) => {
     let json = JSON.stringify(data);
     fs.writeFileSync('test.json', json, 'utf8')
