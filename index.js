bf3fbaa935f8f064b70989fe0956f8a10c95aba3
Woodrex <woodrex83@gmail.com>
2023-01-11 15:43:28 +0800

    fs write json

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import { getFinvizScreen } from "node-finviz-screener";
+import * as fs from 'fs';
 import yahooFinance from "yahoo-finance2";
 
-getFinvizScreen('https://finviz.com/screener.ashx?v=111&f=cap_mid,sh_price_o10')
+
+getFinvizScreen('https://finviz.com/screener.ashx?v=111&f=cap_mid,sh_price_o15')
 .then((res) => {
   Promise.all(res.map(symbol => yahooFinance.quoteSummary(symbol)))
   .then((data) => {
-    console.log(data);
+    let json = JSON.stringify(data);
+    fs.writeFileSync('test.json', json, 'utf8')
   });
 });
 
