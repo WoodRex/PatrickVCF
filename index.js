b9850866ba5dbe4127368f2df54324b63a66f8cf
Woodrex <woodrex83@gmail.com>
2023-01-11 16:53:58 +0800

    beta version

--- index.js
+++ index.js
@@ -5,12 +5,26 @@ import yahooFinance from "yahoo-finance2";
 // Summary detail missing SMA 150
 const queryOptions = { modules: ['price', 'summaryDetail'] };
 
-getFinvizScreen('https://finviz.com/screener.ashx?v=111&f=cap_mid,sh_price_o15')
+getFinvizScreen('https://finviz.com/screener.ashx?v=111&f=cap_mid,sh_price_o10,sh_price_u50')
 .then((res) => {
   Promise.all(res.map(symbol => yahooFinance.quoteSummary(symbol, queryOptions)))
   .then((data) => {
-    let json = JSON.stringify(data);
-    fs.writeFileSync('test.json', json, 'utf8')
+    data.map((d) => {
+      let name = d.price.symbol;
+      let detail = d.summaryDetail;
+
+      let r1 = (detail.regularMarketPreviousClose > detail.fiftyDayAverage);
+      let r2 = (detail.fiftyDayAverage > detail.twoHundredDayAverage);
+      let r3 = ((detail.regularMarketPreviousClose - detail.fiftyTwoWeekLow)/detail.regularMarketPreviousClose) > 0.25;
+      let r4 = (((detail.regularMarketPreviousClose - detail.fiftyTwoWeekHigh)/detail.regularMarketPreviousClose) < 0.15) 
+        && (((detail.regularMarketPreviousClose - detail.fiftyTwoWeekHigh)/detail.regularMarketPreviousClose) > -0.15);
+      
+      if (r1 && r2 && r3 && r4) {
+        console.log(name);
+      }
+    })
+    // let json = JSON.stringify(data);
+    // fs.writeFileSync('test.json', json, 'utf8')
   });
 });
 
