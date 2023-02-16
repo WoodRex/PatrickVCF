55544afa3bd058595948aaee36335a80f6650222
WoodRex <woodrex83@gmail.com>
2023-02-16 23:02:51 +0800

    Changed selector

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import yahooFinance from "yahoo-finance2";
 // Summary detail missing SMA 150
 const queryOptions = { modules: ['price', 'summaryDetail'] };
 
-getFinvizScreen('https://finviz.com/screener.ashx?v=111&f=cap_mid,sh_price_o10,sh_price_u50')
+getFinvizScreen('https://finviz.com/screener.ashx?v=111&f=cap_mid,sh_price_o10,sh_price_u80,ta_perf_13w30o,ta_perf_26w30o')
 .then((res) => {
   Promise.all(res.map(symbol => yahooFinance.quoteSummary(symbol, queryOptions)))
   .then((data) => {
@@ -20,11 +20,23 @@ getFinvizScreen('https://finviz.com/screener.ashx?v=111&f=cap_mid,sh_price_o10,s
         && (((detail.regularMarketPreviousClose - detail.fiftyTwoWeekHigh)/detail.regularMarketPreviousClose) > -0.15);
       
       if (r1 && r2 && r3 && r4) {
-        console.log(name);
+        let oPrice = detail.regularMarketPreviousClose
+        console.log(
+          name,
+          "Buy: ",
+          oPrice.toFixed(2),
+          "Sold L1: ",
+          (oPrice * 1.1).toFixed(2),
+          "Sold L2: ",
+          (oPrice * 1.2).toFixed(2),
+          "Cut Out: ",
+          (oPrice * 0.93).toFixed(2)
+        );
       }
     })
     // let json = JSON.stringify(data);
     // fs.writeFileSync('test.json', json, 'utf8')
   });
 });
+
 
